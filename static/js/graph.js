1d15b05d74716377c89255a34070d75e56aee925
Marta Paz <pazmarta@gmail.com>
2019-01-16 21:58:18 +0000

    add discipline selector

--- static/js/graph.js
+++ static/js/graph.js
@@ -5,9 +5,19 @@ queue()
 function makeGraphs(error, salaryData) {
     var ndx = crossfilter(salaryData);
     
+    show_discipline_selector(ndx);
     show_gender_balance(ndx);
     
     dc.renderAll();
+}
+
+function show_discipline_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('discipline'));
+    var group = dim.group();
+    
+    dc.selectMenu("#discipline-selector")
+        .dimension(dim)
+        .group(group);
 }
 
 function show_gender_balance(ndx) {
@@ -23,7 +33,7 @@ function show_gender_balance(ndx) {
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .elasticY(true)
+        // .elasticY(true)
         .xAxisLabel("Gender")
         .yAxis().ticks(20);
 }
